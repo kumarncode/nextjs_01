878f15f2e878f2c31e5e23f0dbf676d1246185a8
naveen_kumar <naveen_kumar@KUMARNAVEEN>
2026-04-08 11:44:33 +0530

    vercel

--- src/app/utils/config.js
+++ src/app/utils/config.js
@@ -6,7 +6,7 @@
  //production
  const API_BASE_URL = {
      EXPRESS_URL: 'https://ipms.gov.in/node',
- 	TOMCAT_URL: 'https://nextjs-01.vercel.app/api',
+ 	TOMCAT_URL: 'https://nextjs-01-eight.vercel.app/api',
  }
 // const API_BASE_URL = {
 //    TOMCAT_URL: 'http://localhost:3000/api',
